c7ae03353e5b632628240e6a4d6303672285d1e6
Marco Massenzio <m.massenzio@gmail.com>
2014-12-22 18:36:04 -0800

    Updated copyright notice

--- src/main/java/com/alertavert/template/model/IssueType.java
+++ src/main/java/com/alertavert/template/model/IssueType.java
@@ -1,3 +1,6 @@
+// Copyright AlertAvert.com (c) 2014. All rights reserved.
+// Commercial use or modification of this software without a valid license is expressly forbidden
+
 package com.alertavert.template.model;
 
 /**
--- src/test/java/com/alertavert/template/SampleDataRestApplicationTests.java
+++ src/test/java/com/alertavert/template/SampleDataRestApplicationTests.java
@@ -1,5 +1,5 @@
-// Copyright (c) 2014 Elementum SCM, Inc.
-// All rights reserved.  http://www.elementum.com
+// Copyright AlertAvert.com (c) 2014. All rights reserved.
+// Commercial use or modification of this software without a valid license is expressly forbidden
 
 package com.alertavert.template;
 
